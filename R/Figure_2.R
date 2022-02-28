9a77bf8150455831ef54fbe03c8d82c544b01741
Joel Pick <joel.l.pick@gmail.com>
2022-02-28 21:27:22 +0000

    plots

--- R/Figure_2.R
+++ R/Figure_2.R
@@ -66,7 +66,7 @@ boxplot(mean~N_within+N_group+ICC,actual,boxwex=0.5, pch=19, cex=0.5)
 }
 
 setEPS()
-pdf(paste0(wd,"Figures/Fig2.pdf"), height=8, width=8)
+pdf(paste0(wd,"Figures/Fig2.pdf"), height=8, width=10)
 {
 	bias_plot0 <- subset(actual, ICC ==0 & N_within==2)[,1:7]
 	bp0_long<-Stack(bias_plot0, col2stack = c("freq","mean","median","mode"),value.name = "estimate",group.name = "type")
@@ -106,4 +106,34 @@ dev.off()
 ## check ESS
  boxplot(ESS~N_group+N_within+ICC,actual, las=2)
  abline(h=500)
+
+
+bp0_long$bias<-abs(bp0_long$estimate - bp0_long$ICC)
+bp2_long$bias<-abs(bp2_long$estimate - bp2_long$ICC)
+
+{
+	par(mfrow=c(2,1), mar=c(4,4,2,1), cex.axis=0.75, mgp=c(2,0.5,0))
+
+	# boxplot(estimate~N_group+type,bp0_long,boxwex=0.5,xlab="Number of Groups", ylab="Estimate", pch=19, cex=0.5, names=rep(c(20,40,80,160),3),  ylim=c(0,1))#
+	beeswarm(bias~N_group+type,bp0_long, pch=19, cex=0.5, col=alpha(1,0.3),method = "compactswarm",corral="wrap",xlab="Number of Groups", labels=rep(c(20,40,80,160),3), ylab="bias",  ylim=c(0,1))
+	abline(h=0, col="red", lwd=2)
+	abline(v=c(4.5, 8.5, 12.5))
+	mtext(c("ML","Post. Mean","Post. Median","Post. Mode"),side=3,adj=c(0.12,0.375,0.625 ,0.9))
+	mtext("a)",2,padj=-12, las=1, line=2)
+	means<-aggregate(bias~N_group+type,bp0_long,mean)$bias
+	CIs<-aggregate(bias~N_group+type,bp0_long,function(x) sd(x)/sqrt(length(x)))$bias*qnorm(0.975)
+	points(means, pch=19, cex=1.5, col="blue")
+	arrows(1:16,means+CIs,1:16,means-CIs,code=0, angle=90, length=0.05, col="blue", lwd=2)
+
+	# boxplot(bias~N_group+type,bp2_long,boxwex=0.5,xlab="Number of Groups",  ylab="bias", pch=19, cex=0.5, names=rep(c(20,40,80,160),3),  ylim=c(0,1))
+	beeswarm(bias~N_group+type,bp2_long, pch=19, cex=0.5, col=alpha(1,0.3),method = "compactswarm",corral="wrap",xlab="Number of Groups", labels=rep(c(20,40,80,160),3), ylab="bias",  ylim=c(0,1))
+	abline(h=0, col="red", lwd=2)
+	abline(v=c(4.5, 8.5, 12.5))
+	mtext(c("ML","Post. Mean","Post. Median","Post. Mode"),side=3,adj=c(0.12,0.375,0.625 ,0.9))
+	mtext("b)",2,padj=-12, las=1, line=2)
+	means<-aggregate(bias~N_group+type,bp2_long,mean)$bias
+	CIs<-aggregate(bias~N_group+type,bp2_long,function(x) sd(x)/sqrt(length(x)))$bias*qnorm(0.975)
+	points(means, pch=19, cex=1.5, col="blue")
+	arrows(1:16,means+CIs,1:16,means-CIs,code=0, angle=90, length=0.05, col="blue", lwd=2)
+}
 
--- R/Figure_3.R
+++ R/Figure_3.R
@@ -29,27 +29,68 @@ p_perm <- as.data.frame(do.call(rbind,lapply(all, function(z){
 	t(sapply(z, function(x) c(x$param,
 		freq = p_func(x$actual["freq"],x$null[,"freq"]),
 		mean=p_func(x$actual["mean"],x$null[,"mean"]),
+		median=p_func(x$actual["median"],x$null[,"median"]),
 		mode= p_func(x$actual["mode"],x$null[,"mode"])
 		)))
 })))
-p_perm<-subset(p_perm,ICC!=0.8 & N_within!=16)
+# p_perm<-subset(p_perm,ICC!=0.8 & N_within!=16)
+
+
+# setEPS()
+# pdf(paste0(wd,"Figures/Fig3.pdf"), height=6, width=8)
+# ICCs <- c(0,0.2,0.4)
+# {
+# par(mfrow=c(3,1),mar=c(4,4,2,1), cex.axis=0.75, mgp=c(2,0.5,0))
+# for(j in seq_along(ICCs)){
+# 	plot_dat <- subset(p_perm, ICC==ICCs[j])
+# 	# boxplot(mean~N_group + N_within,plot_dat,ylim=c(0,1), ylab=bquote(P[perm]), xlab="Number of Groups",boxwex=0.5, names=rep(c(20,40,80,160),3), pch=19, cex=0.5, col=rep(grey(c(1,0.8,0.6,0.4)),3))
+# 	beeswarm(mean~N_group + N_within,plot_dat, pch=19, cex=0.5, col=alpha(1,0.3),method = "compactswarm",corral="wrap",xlab="Number of Groups", labels=rep(c(20,40,80,160),3), ylab=bquote(P[perm]),  ylim=c(0,1))
+
+# 	abline(v=c(4.5,8.5))
+# 	# abline(h=c(0.25,0.75))
+# 	mtext(c("2","4","8"),side=3,adj=c(0.15,0.5, 0.85), cex=0.8)
+# mtext(paste0(letters[j],")"),2,padj=-5, las=1, line=2)
+
+# }
+# }
+# dev.off()
+
+
+
+p_perm<-subset(p_perm,ICC%in%c(0,0.2) & N_within==2)
 
 
 setEPS()
 pdf(paste0(wd,"Figures/Fig3.pdf"), height=6, width=8)
-ICCs <- c(0,0.2,0.4)
+ICCs <- c(0,0.2)
 {
-par(mfrow=c(3,1),mar=c(4,4,2,1), cex.axis=0.75, mgp=c(2,0.5,0))
+par(mfrow=c(2,1),mar=c(4,4,2,1), cex.axis=0.75, mgp=c(2,0.5,0))
 for(j in seq_along(ICCs)){
 	plot_dat <- subset(p_perm, ICC==ICCs[j])
-	# boxplot(mean~N_group + N_within,plot_dat,ylim=c(0,1), ylab=bquote(P[perm]), xlab="Number of Groups",boxwex=0.5, names=rep(c(20,40,80,160),3), pch=19, cex=0.5, col=rep(grey(c(1,0.8,0.6,0.4)),3))
 	beeswarm(mean~N_group + N_within,plot_dat, pch=19, cex=0.5, col=alpha(1,0.3),method = "compactswarm",corral="wrap",xlab="Number of Groups", labels=rep(c(20,40,80,160),3), ylab=bquote(P[perm]),  ylim=c(0,1))
 
-	abline(v=c(4.5,8.5))
-	# abline(h=c(0.25,0.75))
-	mtext(c("2","4","8"),side=3,adj=c(0.15,0.5, 0.85), cex=0.8)
-mtext(paste0(letters[j],")"),2,padj=-5, las=1, line=2)
+# mtext(paste0(letters[j],")"),2,padj=-5, las=1, line=2)
 
 }
 }
 dev.off()
+
+{
+	power_dat <- aggregate(mean~N_group+ICC,p_perm,function(x) sum(x<=0.05)/100)
+par(mfrow=c(2,1),mar=c(4,4,2,1), cex.axis=0.75, mgp=c(2,0.5,0))
+plot(NA,xlim=c(0,160),xlab="Number of Groups", ylab="Power",  ylim=c(0,1))
+abline(h=0.05, col="grey")
+abline(h=c(0.025,0.075), col="grey")
+for(j in seq_along(ICCs)){
+	points(mean~N_group,power_dat, subset=ICC==ICCs[j], col=j, pch=19)
+	lines(mean~N_group,power_dat, subset=ICC==ICCs[j], col=j, lwd=2)
+}
+}
+
+
+par(mfrow=c(2,2))
+plot(mean~mode,p_perm)
+plot(mean~median,p_perm)
+plot(mean~freq,p_perm)
+plot(mode~freq,p_perm)
+cor(p_perm$mean,p_perm$mode)
