2900f86b9543f5aa59f47bd429816eb51c8a3603
Joel Pick <joel.l.pick@gmail.com>
2022-06-25 09:40:06 +0100

    redo-ing sims

---
+++ R/02_gaussian_sims_perm.R
@@ -0,0 +1,61 @@
+
+rm(list=ls())
+
+devtools::load_all("~/github/squidSim/R")
+
+library(lme4)
+library(parallel)
+library(rstan)
+rstan_options("auto_write" = TRUE)
+options(mc.cores = parallel::detectCores())
+
+wd <- "~/github/bayes_perm/"
+
+source(paste0(wd,"R/00_functions.R"))
+
+LMM_stan <- stan_model(file = paste0(wd,"stan/simple_LMM.stan"))
+pops <- 1:100
+sets <- expand.grid(ICC=c(0,0.1,0.2,0.4),N_group=c(20,40,80),N_within=c(2,4))
+N_perm=100
+
+for(j in 1:nrow(sets)){
+	cat("\n set:",as.matrix(sets[j,]), "\n")
+
+	file_end <- paste0(sets[j,"ICC"],"_",sets[j,"N_group"],"_",sets[j,"N_within"],".Rdata")
+	
+	load(paste0(wd,"Data/Intermediate/gaus_sims_",file_end))
+
+	perm_out <- mclapply(pops,function(i){
+		set.seed(paste0(i,paste(sets[j,c(2,3,1)], collapse="")))
+		cat(i, " ")
+		data <- out[[i]]$data
+
+		perm <- do.call(rbind,lapply(1:N_perm,function(j){
+			data[,"ID"] <- sample(data[,"ID"], replace=FALSE)
+	  	c(gaussian_mods(data)$summary)
+		}))
+
+		list(
+			param = out[[i]]$param,
+			actual = out[[i]]$summary,
+			perm = perm
+		)
+
+	},mc.cores=8)
+
+  # is it exists already, join them together and save the two	
+	file <- paste0("gaus_perm_",file_end)
+	if(file %in% list.files(paste0(wd,"Data/Intermediate/"))){
+		perm_out2 <- perm_out
+		load(paste0(wd,"Data/Intermediate/",file))
+		perm_out <- c(perm_out, perm_out2)
+	}
+
+	save(perm_out, file=paste0(wd,"Data/Intermediate/",file))
+}
+
+
+
+
+#	save(PB_out, file=paste0(wd,"Data/Intermediate/gaus_PB_",file_end))
+
---
+++ R/03_gaussian_sims_boot.R
@@ -0,0 +1,70 @@
+
+rm(list=ls())
+
+devtools::load_all("~/github/squidSim/R")
+
+library(lme4)
+library(parallel)
+library(rstan)
+rstan_options("auto_write" = TRUE)
+options(mc.cores = parallel::detectCores())
+
+wd <- "~/github/bayes_perm/"
+
+source(paste0(wd,"R/00_functions.R"))
+
+LMM_stan <- stan_model(file = paste0(wd,"stan/simple_LMM.stan"))
+pops <- 1:100
+sets <- expand.grid(ICC=c(0,0.1,0.2,0.4),N_group=c(20,40,80),N_within=c(2,4))
+N_boot=100
+
+for(j in 1:length(sets)){
+	cat("\n set:",as.matrix(sets[j,]), "\n")
+
+	file_end <- paste0(sets[j,"ICC"],"_",sets[j,"N_group"],"_",sets[j,"N_within"],".Rdata")
+	
+	load(paste0(wd,"Data/Intermediate/gaus_sims_",file_end))
+
+	boot_out <- mclapply(pops,function(i){
+		set.seed(paste0(i,paste(sets[j,c(2,3,1)], collapse="")))
+		cat(i, " ")
+		data <- out[[i]]$data
+	
+  	ICC <- out[[i]]$ICC
+	 	N_group <- out[[i]]$N_group
+	 	N_within <- out[[i]]$N_within
+		bs_dat_all<-simulate_population(
+			data_structure= make_structure(paste0("ID(",N_group,")"),repeat_obs=N_within),
+			parameters= list(residual=list(vcov=median(rowSums(out[[i]]$post)))),
+			n_pop=N_boot
+		)
+
+		bootstrap <- do.call(rbind,lapply(get_population_data(bs_dat_all, list=TRUE),function(j){
+			
+			c(gaussian_mods(j)$summary)
+
+		}))
+
+	list(
+		param = out[[i]]$parameters,
+		actual=out[[i]]$summary,
+		bootstrap=bootstrap)
+
+	},mc.cores=8)
+
+  # is it exists already, join them together and save the two	
+	file <- paste0("gaus_boot_",file_end)
+	if(file %in% list.files(paste0(wd,"Data/Intermediate/"))){
+		boot_out2 <- boot_out
+		load(paste0(wd,"Data/Intermediate/",file))
+		boot_out <- c(boot_out, boot_out2)
+	}
+
+	save(boot_out, file=paste0(wd,"Data/Intermediate/",file))
+}
+
+
+
+
+#	save(PB_out, file=paste0(wd,"Data/Intermediate/gaus_PB_",file_end))
+
