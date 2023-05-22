36ab2063a31a7aa4a39371971cab22b7a8b81654
Joel Pick <joel.l.pick@gmail.com>
2023-05-22 21:00:33 +0100

    Random regression updates

--- R/p_Z.R
+++ R/p_Z.R
@@ -28,17 +28,15 @@ se_gaus<-as.data.frame(do.call(rbind,lapply(results_gaus, function(i){
 })))
 head(se_gaus)
 
-i=results_gaus[1]
-
-names(out[[1]])
-
-out[[1]]$param
-
 results_gaus <- files[grep("gaus_boot",files)]
 p_gaus<-as.data.frame(do.call(rbind,lapply(results_gaus, function(i){
   load(paste0(wd,"Data/Intermediate/",i))
   t(sapply(boot_out, function(x){
-		c(median2 =as.numeric(x$actual["median"]), median_p = p_func(x$actual["median"],x$boot[,"median"]))
+		c(median2 =as.numeric(x$actual["median"]), 
+			median_p = p_func(x$actual["median"],x$boot[,"median"]),
+			mean_p = p_func(x$actual["mean"],x$boot[,"mean"])
+
+			)
   }))
 })))
 
@@ -55,16 +53,26 @@ results$median_zp <- 2*(1-pnorm(results$z_median))
 results$mean_zp <- 2*(1-pnorm(results$z_mean))
 
 
-power_dat <- aggregate(cbind(median_p,median_zp,mean_zp)~N_group+ N_within+ICC,results,function(x) mean(x<0.05))
+power_dat <- aggregate(cbind(mean_p,median_p,median_zp,mean_zp)~N_group+ N_within+ICC,results,function(x) mean(x<0.05))
 
-plot(median_p~median_zp,power_dat)
+pdf("~/github/null_distributions/Figures/FigSM_z_p.pdf", height=5, width=9)
+{
+par(mfrow=c(1,2))
+
+plot(results$z_mean,results$mean_p, pch=19, cex=0.05, col=scales::alpha(1,0.3), ylab="P-value", xlab="Z-ratio")
+abline(h=0.05, v=qnorm(0.95), col="grey")
+
+plot(mean_p~mean_zp,power_dat, ylab="Power p-value", xlab="Power z-ratio", pch=19)
 abline(0,1)
-#2*(1-pnorm(results$z_median))
+}
+dev.off()
+
+
 {
 par(mfrow=c(2,2))
-	plot((results$z_median),results$median_p, pch=19, cex=0.05, col=scales::alpha(1,0.5))
+	plot(results$z_median,results$median_p, pch=19, cex=0.05, col=scales::alpha(1,0.5))
 	abline(h=0.05, v=qnorm(0.95))
-plot(results$z_mean,results$median_p, pch=19, cex=0.05, col=scales::alpha(1,0.5))
+plot(results$z_mean,results$mean_p, pch=19, cex=0.05, col=scales::alpha(1,0.5))
 abline(h=0.05, v=qnorm(0.95))
 plot(results$z_mode1,results$median_p, pch=19, cex=0.05, col=scales::alpha(1,0.5))
 abline(h=0.05, v=qnorm(0.95))
@@ -72,7 +80,7 @@ plot(results$z_mode0.1,results$median_p, pch=19, cex=0.05, col=scales::alpha(1,0
 abline(h=0.05, v=qnorm(0.95))
 }
 
-cor(results$z,results$median_p)
+cor(log(results$z_median),results$median_p)
 
 {
 par(mfrow=c(2,2))
